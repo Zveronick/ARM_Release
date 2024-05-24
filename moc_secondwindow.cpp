e221b9cfc1355b4e907dede7f169ac337996e29b
Zveronick <Zveronick@mail.ru>
2024-05-24 11:35:41 +0500

    v1.8.2:Fixed new bugs...

--- moc_secondwindow.cpp
+++ moc_secondwindow.cpp
@@ -21,8 +21,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_secondwindow_t {
-    QByteArrayData data[13];
-    char stringdata0[288];
+    QByteArrayData data[11];
+    char stringdata0[251];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -38,20 +38,18 @@ QT_MOC_LITERAL(3, 38, 25), // "on_SaveCustomer_2_clicked"
 QT_MOC_LITERAL(4, 64, 25), // "on_SaveCustomer_3_clicked"
 QT_MOC_LITERAL(5, 90, 29), // "on_restoreTableButton_clicked"
 QT_MOC_LITERAL(6, 120, 24), // "on_SaveCustomer8_clicked"
-QT_MOC_LITERAL(7, 145, 19), // "on_NotAdmin_clicked"
-QT_MOC_LITERAL(8, 165, 16), // "on_Admin_clicked"
-QT_MOC_LITERAL(9, 182, 25), // "on_SaveCustomer_4_clicked"
-QT_MOC_LITERAL(10, 208, 31), // "on_restoreTableButton_2_clicked"
-QT_MOC_LITERAL(11, 240, 26), // "on_SaveCustomer8_2_clicked"
-QT_MOC_LITERAL(12, 267, 20) // "on_ExportCSV_clicked"
+QT_MOC_LITERAL(7, 145, 25), // "on_SaveCustomer_4_clicked"
+QT_MOC_LITERAL(8, 171, 31), // "on_restoreTableButton_2_clicked"
+QT_MOC_LITERAL(9, 203, 26), // "on_SaveCustomer8_2_clicked"
+QT_MOC_LITERAL(10, 230, 20) // "on_ExportCSV_clicked"
 
     },
     "secondwindow\0on_SaveCustomer_clicked\0"
     "\0on_SaveCustomer_2_clicked\0"
     "on_SaveCustomer_3_clicked\0"
     "on_restoreTableButton_clicked\0"
-    "on_SaveCustomer8_clicked\0on_NotAdmin_clicked\0"
-    "on_Admin_clicked\0on_SaveCustomer_4_clicked\0"
+    "on_SaveCustomer8_clicked\0"
+    "on_SaveCustomer_4_clicked\0"
     "on_restoreTableButton_2_clicked\0"
     "on_SaveCustomer8_2_clicked\0"
     "on_ExportCSV_clicked"
@@ -64,7 +62,7 @@ static const uint qt_meta_data_secondwindow[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-      11,   14, // methods
+       9,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -72,21 +70,17 @@ static const uint qt_meta_data_secondwindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   69,    2, 0x08 /* Private */,
-       3,    0,   70,    2, 0x08 /* Private */,
-       4,    0,   71,    2, 0x08 /* Private */,
-       5,    0,   72,    2, 0x08 /* Private */,
-       6,    0,   73,    2, 0x08 /* Private */,
-       7,    0,   74,    2, 0x08 /* Private */,
-       8,    0,   75,    2, 0x08 /* Private */,
-       9,    0,   76,    2, 0x08 /* Private */,
-      10,    0,   77,    2, 0x08 /* Private */,
-      11,    0,   78,    2, 0x08 /* Private */,
-      12,    0,   79,    2, 0x08 /* Private */,
+       1,    0,   59,    2, 0x08 /* Private */,
+       3,    0,   60,    2, 0x08 /* Private */,
+       4,    0,   61,    2, 0x08 /* Private */,
+       5,    0,   62,    2, 0x08 /* Private */,
+       6,    0,   63,    2, 0x08 /* Private */,
+       7,    0,   64,    2, 0x08 /* Private */,
+       8,    0,   65,    2, 0x08 /* Private */,
+       9,    0,   66,    2, 0x08 /* Private */,
+      10,    0,   67,    2, 0x08 /* Private */,
 
  // slots: parameters
-    QMetaType::Void,
-    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
@@ -111,12 +105,10 @@ void secondwindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id
         case 2: _t->on_SaveCustomer_3_clicked(); break;
         case 3: _t->on_restoreTableButton_clicked(); break;
         case 4: _t->on_SaveCustomer8_clicked(); break;
-        case 5: _t->on_NotAdmin_clicked(); break;
-        case 6: _t->on_Admin_clicked(); break;
-        case 7: _t->on_SaveCustomer_4_clicked(); break;
-        case 8: _t->on_restoreTableButton_2_clicked(); break;
-        case 9: _t->on_SaveCustomer8_2_clicked(); break;
-        case 10: _t->on_ExportCSV_clicked(); break;
+        case 5: _t->on_SaveCustomer_4_clicked(); break;
+        case 6: _t->on_restoreTableButton_2_clicked(); break;
+        case 7: _t->on_SaveCustomer8_2_clicked(); break;
+        case 8: _t->on_ExportCSV_clicked(); break;
         default: ;
         }
     }
@@ -152,13 +144,13 @@ int secondwindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 11)
+        if (_id < 9)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 11;
+        _id -= 9;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 11)
+        if (_id < 9)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 11;
+        _id -= 9;
     }
     return _id;
 }
